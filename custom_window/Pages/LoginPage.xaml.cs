400590900b08a29a87e906ea122dbf5d82860809
afmjoaa <afmjoaa@gmail.com>
2019-10-23 20:36:19 +0600

    password dialog box init

--- custom_window/Pages/LoginPage.xaml.cs
+++ custom_window/Pages/LoginPage.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows.Controls;
 using System.Windows.Media;
 using custom_window.Core;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using custom_window.HelperClasses;
 using custom_window.HelperClasses.DataModels;
 
@@ -19,11 +20,43 @@ namespace custom_window.Pages
     {
         private CloudFirestoreService cfService = null;
 
+        #region Constructor
+
         public LoginPage()
         {
             InitializeComponent();
             cfService = CloudFirestoreService.GetInstance();
         }
+
+        #endregion
+
+        #region relay functions
+
+
+        private async void google_OnClick(object sender, RoutedEventArgs e)
+        {
+            //show a custom dialog 
+            await IoC.UI.ShowMessage(new DialogViewModel
+            {
+                Title = "Patient Info Check",
+                Message = "This is the testing message",
+                OkText = "Ok"
+            });
+        }
+
+        private void fb_OnClick(object sender, RoutedEventArgs e)
+        {
+            //testing the patient slide in the main application
+            IoC.Get<ApplicationViewModel>().PatientInfoCheckVisible = true;
+        }
+
+        private void twitter_OnClick(object sender, RoutedEventArgs e)
+        {
+            var Toast = new ToastClass();
+            Toast.ShowNotification("Twitter Authentication", "Twitter authentication is coming soon...", 200);
+        }
+
+        #endregion
 
         #region iconColorFix
 
@@ -171,5 +204,6 @@ namespace custom_window.Pages
         {
            IoC.Get<ApplicationViewModel>().GoToPage(ApplicationPage.Register);
         }
+
     }
 }
