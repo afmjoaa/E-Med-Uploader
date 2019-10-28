2fbed823174fe9abcc7d6b95cd41ef96681b8d0d
zsumon <sumon143014@gmail.com>
2019-10-28 19:20:19 +0600

    registration btn handled

--- custom_window/Controls/PatientContent/NewPatientRegistration.xaml.cs
+++ custom_window/Controls/PatientContent/NewPatientRegistration.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System.Windows.Controls;
+using custom_window.Core;
+using custom_window.HelperClasses;
 
 namespace custom_window
 {
@@ -7,9 +9,38 @@ namespace custom_window
     /// </summary>
     public partial class NewPatientRegistration : UserControl
     {
+        private BarcodeHelper _bar;
+        private FingerprintHelper _fp;
+
         public NewPatientRegistration()
         {
             InitializeComponent();
+            _bar = BarcodeHelper.GetInstance();
+            _fp = FingerprintHelper.GetInstance();
+
+            _bar.modifiedModifiedBarcodeEvent += OnModifiedModifiedBarcodeEvent;
+        }
+
+        private void OnModifiedModifiedBarcodeEvent(string patientname, string oldnid, string newnid,
+            string dateofbirth)
+        {
+            reg_name_tb.Dispatcher.Invoke(new System.Action(()=> {
+                reg_name_tb.Text = patientname;
+            }));
+            // get data for registration   
+            ToastClass.NotifyMin("New data for fingerprint", "getting ready");
+        }
+
+        private void Register_Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            IoC.Get<ApplicationViewModel>().GoToPage(ApplicationPage.Home);
+           // _bar.modifiedModifiedBarcodeEvent -= OnModifiedModifiedBarcodeEvent;
+        }
+
+        private void Skip_Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            IoC.Get<ApplicationViewModel>().GoToPage(ApplicationPage.Home);
+           // _bar.modifiedModifiedBarcodeEvent -= OnModifiedModifiedBarcodeEvent;
         }
     }
 }
